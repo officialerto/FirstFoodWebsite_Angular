fe947c71fb8629e89e70ae700f7e753feacb7908
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-09-12 21:59:56 +0300

    FoodMineWebsite_Angular

---
+++ foodmine-course/src/app/services/food/food.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FoodService {
+
+  constructor() { }
+
+  getAll():String[]{
+    return[
+      '/assets/images/foods/1.jpg',
+      '/assets/images/foods/2.jpg',
+      '/assets/images/foods/3.jpg',
+      '/assets/images/foods/4.jpg',
+      '/assets/images/foods/5.jpg'
+    ]
+  }
+}
